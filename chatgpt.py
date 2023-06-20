e044c2a7630489bec95233672a0aa085345ce6bc
PatrickAcheson <patrick.acheson@hotmail.com>
2023-06-20 16:46:44 +0100

    starting point

---
+++ chatgpt.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import constants
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+os.envi
+
+
