8452c393836f3971f005ed5002324a2a1c8db67a
PatrickAcheson <patrick.acheson@hotmail.com>
2023-06-20 16:50:04 +0100

    code

--- chatgpt.py
+++ chatgpt.py
@@ -7,6 +7,14 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 
-os.envi
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+query = sys.argv[1]
+
+loader = TextLoader('data.txt')
+#loader = DirectoryLoader(".", glob="*.txt")
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+print(index.query(query, llm=ChatOpenAI))
 
 
